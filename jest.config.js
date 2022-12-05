4c6291c63c5ad2e5cbdd195a72793442a4293920
Anton Bessonov <anton.bessonov.public@gmail.com>
2022-12-05 22:20:57 +0000

    update micro example

--- jest.config.js
+++ jest.config.js
@@ -7,18 +7,15 @@ module.exports = {
 	testRegex: '(/__tests__/.*|(\\.|/)test)\\.tsx?$',
 	testPathIgnorePatterns: ['/node_modules/'],
 	moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+	transform: {
+		'\\.[jt]sx?$': ['ts-jest'],
+	},
 	coverageThreshold: {
 		global: {
 			statements: 100,
 			branches: 100,
 			functions: 100,
 			lines: 100,
-		},
-	},
-	// https://github.com/kulshekhar/ts-jest/issues/823#issuecomment-515529012
-	globals: {
-		'ts-jest': {
-			packageJson: 'package.json',
 		},
 	},
 }
